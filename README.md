c6edf294ba7bed170b32a06089bafbcf89bdb28d
John Paul <john@johnkpaul.com>
2013-05-30 11:45:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# node-geolocate
+
+Use geolocation services to get lat long.  Currently only works on OSX Snow Leopard or greater, but I'd love contributions for other platforms.
+
+
+```javascript
+
+var geolocate = require('node-geolocate');
+geolocate(function(latLong){
+   var latitude = latLong[0];
+   var longitude = latLong[1];
+});
+
+```
+
+Thanks to https://github.com/robmathers/WhereAmI for the actual heavy lifting!
---
+++ index.js
@@ -0,0 +1,14 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+exports = module.exports = function geolocate(callback){
+
+exec("./bin/whereami | head -n2 | sed 's/Latitude: //' | sed 's/Longitude: //'", function (error, stdout, stderr) {
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+
+  //calls callback with [LAT, LONG]
+  callback(stdout.split('\n').map(parseFloat).splice(0,2));
+}); 
+}
